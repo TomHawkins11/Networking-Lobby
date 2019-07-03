0b5ae8b449b95928fb8bc08a9f8ab0755a35f376
Tom Hawkins <tks.hawkins11@gmail.com>
2019-07-03 21:01:18 +1000

    Added Server Folder

---
+++ Server/Server - Connection Lobby/Assets/Scripts/JoinButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking.Match;
+using UnityEngine.UI;
+
+public class JoinButton : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    private Text buttonText;
+    private MatchInfoSnapshot match;
+    private void Awake()
+    {
+        buttonText = GetComponentInChildren<Text>();
+        GetComponent<Button>().onClick.AddListener(JoinMatch);
+    }
+
+    
+    public void Inititialize(MatchInfoSnapshot match, Transform panelTransform)
+    {
+        this.match = match;
+        buttonText.text = match.name;
+        transform.SetParent(panelTransform);
+        transform.localScale = Vector3.one;
+        transform.localRotation = Quaternion.identity;
+        transform.localPosition = Vector3.zero;
+    }
+
+    public void JoinMatch()
+    {
+        FindObjectOfType<CustomNetworkManager>().JoinGame(match);
+    }
+}
---
+++ Server/Server - Connection Lobby/Assets/Scripts/testScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    NetworkDiscovery nd;
+    void Start()
+    {
+        nd = gameObject.GetComponent<NetworkDiscovery>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(nd.broadcastData);
+        Debug.Log(nd.broadcastKey);
+        Debug.Log(nd.broadcastPort);
+
+    }
+}
